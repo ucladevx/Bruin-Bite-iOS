7ac4d7714fc94f9cd99fe60dc2f46cdb7e5577dc
Ayush Patel <25682019+ayush-patel@users.noreply.github.com>
2018-02-03 14:07:46 -0800

    -- ColoredCircle as an example to build the activity indicator

---
+++ Dont Eat Alone/ColoredCircle.swift
@@ -0,0 +1,57 @@
+//
+//  ColoredCircle.swift
+//  Dont Eat Alone
+//
+//  Created by Ayush Patel on 2/3/18.
+//  Copyright © 2018 Dont Eat Alone. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class ColoredCircle: UIView {
+    
+    var scale: CGFloat = 0.9
+    @IBInspectable
+    var color: UIColor = .blue{
+        didSet{
+            setNeedsDisplay()
+        }
+    }
+    
+    private var circleCenter: CGPoint{
+        return CGPoint(x: bounds.midX, y: bounds.midY)
+    }
+    
+    private var circleRadius: CGFloat{
+        return min(bounds.size.width, bounds.size.height)/2 * scale
+    }
+    
+    private func pathForCircle() -> UIBezierPath {
+        let path = UIBezierPath(
+            arcCenter: circleCenter,
+            radius: circleRadius,
+            startAngle: 0,
+            endAngle: CGFloat.pi * 2,
+            clockwise: true
+        )
+        return path
+    }
+    
+    override func draw(_ rect: CGRect) {
+        color.set()
+        pathForCircle().fill()
+        
+    }
+    
+    //    init(frame: CGRect, color: UIColor) {
+    //        self.color = color
+    //        super.init(frame: frame)
+    //    }
+    //
+    //    required init?(coder aDecoder: NSCoder) {
+    //        self.color = .blue
+    //        super.init(coder: aDecoder)
+    //    }
+}
+
