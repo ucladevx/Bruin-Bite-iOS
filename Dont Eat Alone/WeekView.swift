7b153e34e8072bcf9bf644dc96b27f77c9407a22
Arpit Jasapara <lordtipra@gmail.com>
2018-02-07 17:38:10 -0800

    Added collection view and updated elements

--- Dont Eat Alone/WeekView.swift
+++ Dont Eat Alone/WeekView.swift
@@ -8,22 +8,27 @@
 
 import UIKit
 @IBDesignable
+
+protocol WeekViewTappedDelegate {
+    func daySelected(_ selectedLabelText: String)
+}
+
 class WeekView: UIView {
     @IBOutlet var dateLbl: UILabel!
     
     var tapGesture: UITapGestureRecognizer?
-    var delegate: DayViewTappedDelegate?
+    var delegate: WeekViewTappedDelegate?
     
-    func setDelegateAndTap(_ delegate: DayViewTappedDelegate) {
+    func setDelegateAndTap(_ delegate: WeekViewTappedDelegate) {
         self.delegate = delegate
-        self.tapGesture = UITapGestureRecognizer(target: self, action: #selector(self.dayViewTapped))
+        self.tapGesture = UITapGestureRecognizer(target: self, action: #selector(self.weekViewTapped))
         tapGesture?.numberOfTapsRequired = 1
         
         self.addGestureRecognizer(tapGesture!)
     }
     
     @objc
-    func dayViewTapped() {
+    func weekViewTapped() {
         delegate!.daySelected(self.dateLbl.text!)
     }
 
