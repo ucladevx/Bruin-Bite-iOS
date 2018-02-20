a8e8efebbac715be3a2bfbd1ed2deec73943f0c3
Ayush Patel <25682019+ayush-patel@users.noreply.github.com>
2018-02-19 18:15:32 -0800

    -- started setting up the data model

---
+++ Dont Eat Alone/Models/Menu.swift
@@ -0,0 +1,65 @@
+//
+//  Menu.swift
+//  Dont Eat Alone
+//
+//  Created by Ayush Patel on 2/19/18.
+//  Copyright © 2018 Dont Eat Alone. All rights reserved.
+//
+
+import Foundation
+
+enum Allergens{
+    case None
+    case Vegetarian
+    case Vegan
+    case ContainsPeanuts
+    case ContainsTreeNuts
+    case ContainsWheat
+    case ContainsSoy
+    case ContainsDairy
+    case ContainsEggs
+    case ContainsShellfish
+    case ContainsFish
+}
+
+enum MealPeriod: String{
+    case breakfast = "Breakfast"
+    case lunch = "Lunch"
+    case dinner = "Dinner"
+    case lateNight = "Late Night"
+    case brunch = "Brunch"
+}
+
+enum Location: String{
+    case deNeve = "De Neve"
+    case covel = "Covel"
+    case bPlate = "BPlate"
+    case feast = "Feast"
+}
+
+
+struct Item{
+    var name: String
+    var allergies: [Allergens]
+    var recipeLink: String?
+    //add nutrition here later
+}
+
+struct Menu{
+    var mealPeriod: MealPeriod
+    var location: Location
+    var item: Item
+}
+
+class MenuController{
+    var abc: String
+    init(abc: String){
+        self.abc = abc
+    }
+    
+    //Menu
+    //  -minimized menu items
+    //  -get activity levels
+    //  -get times
+    //  -
+}
