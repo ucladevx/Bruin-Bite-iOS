b2add0d7909653220278eb81db97ae34dd648fa9
Ayush Patel <25682019+ayush-patel@users.noreply.github.com>
2018-03-11 17:05:27 -0700

    -- started working on bottom allergy bar

---
+++ Dont Eat Alone/Views/AllergensBarScrollView.swift
@@ -0,0 +1,15 @@
+//
+//  AllergensBarScrollView.swift
+//  Dont Eat Alone
+//
+//  Created by Ayush Patel on 3/11/18.
+//  Copyright © 2018 Dont Eat Alone. All rights reserved.
+//
+
+import UIKit
+import SnapKit
+
+//class AllergensBarScrollView: UIScrollView {
+//    
+//}
+
