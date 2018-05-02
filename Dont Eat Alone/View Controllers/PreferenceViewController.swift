faa5eb82516a24754b2f1cfbae93c71ef9387c9e
Ryan Lee <ryanlee@s-128-97-179-200.resnet.ucla.edu>
2018-05-01 21:43:21 -0700

    Added Date and Time preference popup views

--- Dont Eat Alone/View Controllers/PreferenceViewController.swift
+++ Dont Eat Alone/View Controllers/PreferenceViewController.swift
@@ -11,11 +11,11 @@ import CZPicker
 
 class PreferenceViewController: UIViewController {
 
+    @IBOutlet weak var timeField: UITextField!
     @IBOutlet weak var dateField: UITextField!
     @IBOutlet weak var diningField: UITextField!
-    let diningpicker = CZPickerView(headerTitle: "Dining Hall", cancelButtonTitle: "Cancel", confirmButtonTitle: "Confirm")
+    var picks = [String]()
     
-    let dininghalls = ["Covel", "De Neve", "Feast", "Bruin Plate"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,6 +23,8 @@ class PreferenceViewController: UIViewController {
         diningField.textColor = UIColor.darkGray
         dateField.font = UIFont(name: "Avenir", size: 18)
         dateField.textColor = UIColor.darkGray
+        timeField.font = UIFont(name: "Avenir", size: 18)
+        timeField.textColor = UIColor.darkGray
         
     }
     
@@ -32,27 +34,67 @@ class PreferenceViewController: UIViewController {
     }
     
     @IBAction func showWithMultipleSelections(sender: AnyObject) {
-        diningpicker?.delegate = self as CZPickerViewDelegate
-        diningpicker?.dataSource = self as CZPickerViewDataSource
-        diningpicker?.needFooterView = false
-        diningpicker?.allowMultipleSelection = true
-        diningpicker?.show()
+        let picker = CZPickerView(headerTitle: "Dining Halls", cancelButtonTitle: "Cancel", confirmButtonTitle: "Confirm")
+        
+        picks = ["Covel", "De Neve", "Feast", "Bruin Plate"]
+        picker?.delegate = self as CZPickerViewDelegate
+        picker?.dataSource = self as CZPickerViewDataSource
+        picker?.needFooterView = false
+        picker?.allowMultipleSelection = true
+        picker?.checkmarkColor = UIColor.deaScarlet
+        picker?.headerBackgroundColor = UIColor.deaScarlet
+        picker?.confirmButtonBackgroundColor = UIColor.deaScarlet
+        picker?.cancelButtonBackgroundColor = UIColor.deaScarlet
+        picker?.cancelButtonNormalColor = UIColor.white
+        picker?.show()
     }
+    
+    @IBAction func selectDate(sender: AnyObject) {
+        let picker = CZPickerView(headerTitle: "Dates", cancelButtonTitle: "Cancel", confirmButtonTitle: "Confirm")
+        
+        picks = ["May 2, 2018", "May 3, 2018", "May 4, 2018", "May 5, 2018"]
+        picker?.delegate = self as CZPickerViewDelegate
+        picker?.dataSource = self as CZPickerViewDataSource
+        picker?.needFooterView = false
+        picker?.allowMultipleSelection = true
+        picker?.checkmarkColor = UIColor.deaScarlet
+        picker?.headerBackgroundColor = UIColor.deaScarlet
+        picker?.confirmButtonBackgroundColor = UIColor.deaScarlet
+        picker?.cancelButtonBackgroundColor = UIColor.deaScarlet
+        picker?.cancelButtonNormalColor = UIColor.white
+        picker?.show()
+    }
+    
+    @IBAction func selectTime(sender: AnyObject) {
+        let picker = CZPickerView(headerTitle: "Times", cancelButtonTitle: "Cancel", confirmButtonTitle: "Confirm")
+        
+        picks = ["9:15pm", "9:30pm", "9:45pm", "10:00pm"]
+        picker?.delegate = self as CZPickerViewDelegate
+        picker?.dataSource = self as CZPickerViewDataSource
+        picker?.needFooterView = false
+        picker?.allowMultipleSelection = true
+        picker?.checkmarkColor = UIColor.deaScarlet
+        picker?.headerBackgroundColor = UIColor.deaScarlet
+        picker?.confirmButtonBackgroundColor = UIColor.deaScarlet
+        picker?.cancelButtonBackgroundColor = UIColor.deaScarlet
+        picker?.cancelButtonNormalColor = UIColor.white
+        picker?.show()
+    }
+    
 }
-
 
 extension PreferenceViewController: CZPickerViewDelegate, CZPickerViewDataSource {
         func numberOfRows(in pickerView: CZPickerView!) -> Int {
-            return dininghalls.count
+            return picks.count
         }
     
     
     func czpickerView(_ pickerView: CZPickerView!, titleForRow row: Int) -> String! {
-            return dininghalls[row]
+            return picks[row]
         }
         
     func czpickerView(_ pickerView: CZPickerView!, didConfirmWithItemAtRow row: Int){
-        print(dininghalls[row])
+        print(picks[row])
             self.navigationController?.setNavigationBarHidden(true, animated: true)
             
         }
@@ -65,13 +107,25 @@ extension PreferenceViewController: CZPickerViewDelegate, CZPickerViewDataSource
         var chosen = String()
         var i = 1
         for row in rows {
-            chosen += dininghalls[row as! Int]
+            chosen += picks[row as! Int]
             if i != rows.count {
                 chosen += ", "
             }
             i += 1
         }
-        diningField.text = chosen
+        
+        switch(picks[0]) {
+        case "Covel":
+            diningField.text = chosen
+            break;
+        case "9:15pm":
+            timeField.text = chosen
+            break;
+        default:
+            dateField.text = chosen
+            break;
+        }
+        
     }
     
     /*
