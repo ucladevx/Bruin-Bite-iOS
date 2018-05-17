d581e89b432dc52f840436ef41a7fa448e0f005f
kamalca <sonca1999@outlook.com>
2018-05-16 21:40:38 -0700

    Matching Screens created
    Figma 8.10, 8.27, 8.41 created
    Pulsator Animation implemented

---
+++ Dont Eat Alone/View Controllers/NoMatchViewController.swift
@@ -0,0 +1,40 @@
+//
+//  NoMatchViewController.swift
+//  Dont Eat Alone
+//
+//  Created by Kameron Carr on 5/16/18.
+//  Copyright © 2018 Dont Eat Alone. All rights reserved.
+//
+
+import UIKit
+
+class NoMatchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let destination = segue.destination as? SearchingScreenViewController {
+            destination.puls.start() 
+        }
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
