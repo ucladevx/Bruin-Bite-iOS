b0ec4e3f6118d770cad628121b6bf89fac81d14c
sjl1826 <heysamlee@gmail.com>
2018-05-21 09:45:18 -0700

    added new networking files

--- Dont Eat Alone/Network/UserAPI.swift
+++ Dont Eat Alone/Network/UserAPI.swift
@@ -10,16 +10,16 @@ import Foundation
 import Result
 
 extension API {
-
+    
     static let UserAPIKey = "myKey"
     
-    static func createUser(email: String, password: String, first_name: String, last_name: String, major: String, minor: String, year: Int, self_bio: String, completion: @escaping () -> Void) {
+    static func createUser(email: String, password: String, first_name: String, last_name: String, major: String, minor: String, year: Int, self_bio: String, completion: @escaping (UserCreate) -> ()) {
         provider.request(.createUser(email: email, password: password, first_name: first_name, last_name: last_name, major: major, minor: minor, year: year, self_bio: self_bio)) { result in
             switch result {
             case let .success(response):
                 do {
-                    let results = try JSONDecoder().decode( UserCreate.self, from: response.data)
-                    completion()
+                    let results = try JSONDecoder().decode(UserCreate.self, from: response.data)
+                    completion(results)
                 } catch let err {
                     print(err)
                 }
@@ -29,7 +29,7 @@ extension API {
         }
     }
     
-    static func loginUser(username: String, password: String, grant_type: String, client_id: String, client_secret: String, completion: @escaping () -> Void) {
+    static func loginUser(username: String, password: String, grant_type: String, client_id: String, client_secret: String, completion: @escaping (UserLog) -> ()) {
         USEREMAIL = username
         provider.request(.loginUser(username:username, password: password, grant_type: grant_type, client_id: client_id, client_secret: client_secret)) { result in
             switch result {
@@ -37,25 +37,24 @@ extension API {
                 do {
                     let results = try JSONDecoder().decode(UserLog.self, from: response.data)
                     UserDefaults.standard.set(results.access_token, forKey: username)
-                    completion()
+                    completion(results)
                 } catch let err {
                     print(err)
                 }
             case let .failure(error):
                 print(error)
             }
-
+            
         }
     }
     
-   static func readUsers(email: String, access_token: String, completion: @escaping () -> Void) {
+    static func readUsers(email: String, access_token: String, completion: @escaping (UserCreate) -> ()) {
         API.provider.request(.readUsers(email: email, access_token: access_token)) { result in
             switch result {
             case let .success(response):
                 do {
                     let results = try JSONDecoder().decode(UserCreate.self, from: response.data)
-                    print(results)
-                    completion()
+                    completion(results)
                 } catch let err {
                     print(err)
                 }
@@ -65,14 +64,13 @@ extension API {
         }
     }
     
-   static func updateUser(email: String, password: String, first_name: String, last_name: String, major: String, minor: String, year: Int, self_bio: String, access_token: String, completion: @escaping () -> Void) {
+    static func updateUser(email: String, password: String, first_name: String, last_name: String, major: String, minor: String, year: Int, self_bio: String, access_token: String, completion: @escaping (UserCreate) -> ()) {
         provider.request(.updateUser(email: email, password: password, first_name: first_name, last_name: last_name, major: major, minor: minor, year: year, self_bio: self_bio, access_token: access_token)) { result in
             switch result {
             case let .success(response):
                 do {
                     let results = try JSONDecoder().decode(UserCreate.self, from: response.data)
-                    print(results)
-                    completion()
+                    completion(results)
                 } catch let err {
                     print(err)
                 }
@@ -84,16 +82,16 @@ extension API {
     
     static func deleteUser(email: String, access_token: String, completion: @escaping () -> Void) {
         provider.request(.deleteUser(email:email, access_token: access_token)) { result in
-        switch result {
-        case let .success(response):
+            switch result {
+            case let .success(response):
                 print("Delete: \(response)")
-            completion()
-        case let .failure(error):
-            print(error)
+                completion()
+            case let .failure(error):
+                print(error)
             }
             UserDefaults.standard.removeObject(forKey: email)
         }
     }
-
+    
 }
 
