3ff6398ebb8fa4c636d8ef7847f55b627b48a964
Ayush Patel <25682019+ayush-patel@users.noreply.github.com>
2018-05-24 21:28:04 -0700

    fixed the constraints for profile VC by adding a static table view

--- Dont Eat Alone/UserClass.swift
+++ Dont Eat Alone/UserClass.swift
@@ -104,18 +104,18 @@ public class User {
         }
     }
     public func readUser() {
-        DispatchQueue.global(qos: .background).async {
-            API.readUsers(email: self.user_email, access_token: MAIN_USER.getToken()!) { (sent_user) in
-                self.create_read_update = sent_user
-                self.user_email = self.create_read_update?.email ?? ""
-                self.first_name = self.create_read_update?.first_name ?? ""
-                self.last_name = self.create_read_update?.last_name ?? ""
-                self.user_major = self.create_read_update?.major ?? ""
-                self.user_minor = self.create_read_update?.minor ?? ""
-                self.user_bio = self.create_read_update?.self_bio ?? ""
-                self.user_year = self.create_read_update?.year ?? 0
-            }
-        }
+//        DispatchQueue.global(qos: .background).async {
+//            API.readUsers(email: self.user_email, access_token: MAIN_USER.getToken()!) { (sent_user) in
+//                self.create_read_update = sent_user
+//                self.user_email = self.create_read_update?.email ?? ""
+//                self.first_name = self.create_read_update?.first_name ?? ""
+//                self.last_name = self.create_read_update?.last_name ?? ""
+//                self.user_major = self.create_read_update?.major ?? ""
+//                self.user_minor = self.create_read_update?.minor ?? ""
+//                self.user_bio = self.create_read_update?.self_bio ?? ""
+//                self.user_year = self.create_read_update?.year ?? 0
+//            }
+//        }
     }
     public func updateUser() {
         DispatchQueue.global(qos: .background).async {
