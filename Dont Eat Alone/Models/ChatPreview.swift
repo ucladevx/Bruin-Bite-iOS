a0ebe474d4795ca60fe3e76f99d6bbaf1f8d9334
HirdayGupta <hirday.gupta@gmail.com>
2018-05-26 05:29:29 -0700

    Add chatlist VC and replace w storyboard reference in main SB

---
+++ Dont Eat Alone/Models/ChatPreview.swift
@@ -0,0 +1,18 @@
+//
+//  ChatPreviewModel.swift
+//  Dont Eat Alone
+//
+//  Created by Hirday Gupta on 26/05/18.
+//  Copyright © 2018 Dont Eat Alone. All rights reserved.
+//
+
+import Foundation
+
+struct ChatPreview {
+    var name: String
+    var date: String
+    var meal: String
+    var time: String
+    var profileImage: String
+    var unreadMessage: Int
+}
