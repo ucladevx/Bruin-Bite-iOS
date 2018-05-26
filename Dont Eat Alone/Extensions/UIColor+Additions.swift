181cf0ef8c28993dfafd34532fa956e5715553f5
HirdayGupta <hirday.gupta@gmail.com>
2018-05-26 01:05:39 -0700

    Ayush breaks things (I fix them)

--- Dont Eat Alone/Extensions/UIColor+Additions.swift
+++ Dont Eat Alone/Extensions/UIColor+Additions.swift
@@ -42,4 +42,8 @@ extension UIColor {
         return UIColor(white: 111.0 / 255.0, alpha: 1.0)
     }
 
+    @nonobjc class var profileOptionGray: UIColor {
+        return UIColor(red: 128.0 / 255.0, green: 128.0 / 255.0, blue: 128.0 / 255.0, alpha: 1.0)
+    }
+
 }
