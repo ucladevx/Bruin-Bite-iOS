fd548c17062e04a31982d552ca98274a401d4b65
Cyncia <cynciazhou@gmail.com>
2018-05-25 23:59:34 -0700

    changed activity level bar to one color only depending on the percentage populated

--- Dont Eat Alone/Views/Menu/ActivityLevelBar.swift
+++ Dont Eat Alone/Views/Menu/ActivityLevelBar.swift
@@ -49,44 +49,43 @@ class ActivityLevelBar: UIView {
         let width = UIScreen.main.bounds.size.width
         
             if(self.percentage > 0.8) {
-                let start_width = 0.8*width
-                let percent_filled = (self.percentage - 0.8)*width
+                let percent_filled = (self.percentage)*width
                 
                 ctx.setFillColor(UIColor.deaScarlet.cgColor)
-                ctx.addPath(UIBezierPath(roundedRect: CGRect(x: 0, y: 0, width: CGFloat(start_width+percent_filled), height: 10), byRoundingCorners: [], cornerRadii: CGSize(width: 6, height: 6)).cgPath)
-                
+                // before was start_width + percent_filled
+                ctx.addPath(UIBezierPath(roundedRect: CGRect(x: 0, y: 0, width: CGFloat(percent_filled), height: 10), byRoundingCorners: [], cornerRadii: CGSize(width: 6, height: 6)).cgPath)
                 
                 ctx.fillPath()
                 
             }
-            if(self.percentage > 0.6) {
+        
+            else if(self.percentage > 0.6) {
                 ctx.setFillColor(UIColor.deaYellow.cgColor)
-                
-                let start_width = 0.6*width
                 var percent_filled: CGFloat
                 if(self.percentage > 0.8) {
                     percent_filled = 0.2*width
                 }
                 else {
-                    percent_filled = (self.percentage - 0.6)*width
+                    percent_filled = (self.percentage)*width
                 }
-                ctx.addPath(UIBezierPath(roundedRect: CGRect(x: 0, y: 0, width: start_width + percent_filled, height: 10), byRoundingCorners: [], cornerRadii: CGSize(width: 6, height: 6)).cgPath)
+                ctx.addPath(UIBezierPath(roundedRect: CGRect(x: 0, y: 0, width: percent_filled, height: 10), byRoundingCorners: [], cornerRadii: CGSize(width: 6, height: 6)).cgPath)
                 ctx.fillPath()
             }
             
             var percent_filled: CGFloat = 0
+        
             if(self.percentage > 0.6) {
                 percent_filled = 1
             }
+                
             else {
                 percent_filled = self.percentage/0.6
+                ctx.setFillColor(UIColor.deaAppleGreen.cgColor)
+                ctx.addPath(UIBezierPath(roundedRect: CGRect(x: 0, y: 0, width: 0.6*width*percent_filled, height: 10), byRoundingCorners: [], cornerRadii: CGSize(width: 6, height: 6)).cgPath)
+                ctx.fillPath()
             }
-            ctx.setFillColor(UIColor.deaAppleGreen.cgColor)
-            ctx.addPath(UIBezierPath(roundedRect: CGRect(x: 0, y: 0, width: 0.6*width*percent_filled, height: 10), byRoundingCorners: [], cornerRadii: CGSize(width: 6, height: 6)).cgPath)
-            //
-            
+ 
             ctx.closePath()
-            ctx.fillPath()
             ctx.restoreGState()
     }
     
