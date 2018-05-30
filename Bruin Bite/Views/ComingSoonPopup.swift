c440f8bc0160072138fa609479c63cdba8279c93
Ayush Patel <25682019+ayush-patel@users.noreply.github.com>
2018-05-30 02:45:39 -0700

    -- added coming soon popup view

---
+++ Bruin Bite/Views/ComingSoonPopup.swift
@@ -0,0 +1,31 @@
+//
+//  ComingSoonPopup.swift
+//  Bruin Bite
+//
+//  Created by Ayush Patel on 5/30/18.
+//  Copyright © 2018 Dont Eat Alone. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class ComingSoonPopup: UIView {
+    @IBOutlet var contenView: UIView!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        commonInit()
+    }
+    
+    private func commonInit(){
+        Bundle.main.loadNibNamed("ComingSoonPopup", owner: self, options: nil)
+        addSubview(contenView)
+        contenView.frame = self.bounds
+        contenView.autoresizingMask = [.flexibleHeight, .flexibleWidth]
+    }
+}
