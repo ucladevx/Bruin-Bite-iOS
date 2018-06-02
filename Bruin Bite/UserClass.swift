5b719dfc99f21720c7fa82d4573a97e1d405baa5
sjl1826 <heysamlee@gmail.com>
2018-06-01 20:18:17 -0700

    fixed force unwrap for create and match user

--- Bruin Bite/UserClass.swift
+++ Bruin Bite/UserClass.swift
@@ -172,7 +172,7 @@ public class User {
     }
     public func readUser() {
         DispatchQueue.global(qos: .background).async {
-            API.readUsers(email: self.user_email, access_token: MAIN_USER.getToken()!) { (sent_user) in
+            API.readUsers(email: self.user_email, access_token: MAIN_USER.getToken() ?? "") { (sent_user) in
                 self.create_read_update = sent_user
                 self.user_email = self.create_read_update?.email ?? ""
                 self.first_name = self.create_read_update?.first_name ?? ""
@@ -187,14 +187,14 @@ public class User {
     }
     public func updateUser(dev_id: String) {
         DispatchQueue.global(qos: .background).async {
-            API.updateUser(email: self.user_email, password: self.user_password, first_name: self.first_name, last_name: self.last_name, major: self.user_major, minor: self.user_minor, year: self.user_year, self_bio: self.user_bio, access_token: UserDefaults.standard.object(forKey: self.user_email) as! String, device_id: dev_id) { (updatedUser) in
+            API.updateUser(email: self.user_email, password: self.user_password, first_name: self.first_name, last_name: self.last_name, major: self.user_major, minor: self.user_minor, year: self.user_year, self_bio: self.user_bio, access_token: UserDefaults.standard.object(forKey: self.user_email) as? String ?? "", device_id: dev_id) { (updatedUser) in
                 self.create_read_update = updatedUser
             }
         }
     }
     public func deleteUser() {
         DispatchQueue.global(qos: .background).async {
-            API.deleteUser(email: self.user_email, access_token: UserDefaults.standard.object(forKey: self.user_email) as! String) {
+            API.deleteUser(email: self.user_email, access_token: UserDefaults.standard.object(forKey: self.user_email) as? String ?? "") {
                 print("Deleted User")
             }
         }
