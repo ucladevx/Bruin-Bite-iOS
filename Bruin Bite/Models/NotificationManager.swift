cfbe6f369bef095e802f134ed9014fbec74117b5
kamalca <sonca1999@outlook.com>
2018-09-02 11:15:39 -0700

    Popups working
    Got some basic popups working, but no design has gone into it yet.

--- Bruin Bite/Models/NotificationManager.swift
+++ Bruin Bite/Models/NotificationManager.swift
@@ -23,5 +23,16 @@ class NotificationManager {
     func banner(type: GSMessageType, message: String, to view: UIViewController) {
         view.showMessage(message, type: type)
     }
-    
+    func popup(title: String, subtitle: String) {
+        var attributes = EKAttributes.centerFloat
+        attributes.entryBackground = .gradient(gradient: .init(colors: [.red, .green], startPoint: .zero, endPoint: CGPoint(x: 1, y: 1)))
+        let EKTitle = EKProperty.LabelContent(text: title, style: .init(font: UIFont.boldSystemFont(ofSize: 21), color: UIColor.black))
+        let EKSubTitle = EKProperty.LabelContent(text: subtitle, style: .init(font: UIFont.boldSystemFont(ofSize: 21), color: UIColor.black))
+        let button = EKProperty.ButtonContent(label: EKProperty.LabelContent(text: "Dismiss", style: .init(font: UIFont.boldSystemFont(ofSize: 18), color: UIColor.black)), backgroundColor: UIColor.white, highlightedBackgroundColor: UIColor.red)
+        let popup = EKPopUpMessage(topImage: EKProperty.ImageContent(image: UIImage()), title: EKTitle, description: EKSubTitle, button: button) {
+            SwiftEntryKit.dismiss()
+        }
+        let contentView = EKPopUpMessageView(with: popup)
+        SwiftEntryKit.display(entry: contentView, using: attributes)
+    }
 }
