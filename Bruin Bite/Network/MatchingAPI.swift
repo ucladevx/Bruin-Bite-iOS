76b31677617a841c71a40f3abb3964068b9d2109
kamalca <sonca1999@outlook.com>
2019-03-14 18:31:32 -0700

    Better Test Data
    Made the test data more realistic

--- Bruin Bite/Network/MatchingAPI.swift
+++ Bruin Bite/Network/MatchingAPI.swift
@@ -10,30 +10,82 @@ import Foundation
 import UIKit
 func getSuccessfulMatches(completionHandler: ([SuccessfulMatch])->Void){
     var successfulMatches = [SuccessfulMatch]()
-    for i in 1...4 {
-        var match = SuccessfulMatch(profilePicture: UIImage(), user: String(), firstName: String(), lastName: String(), diningHall: String(), mealPeriod: String(), chatURL: String(), day: String(), time: String())
-        match.user = "User" + "\(i)"
-        match.firstName = "FN" + "\(i)"
-        match.lastName = "LN" + "\(i)"
-        match.diningHall = "BP"
-        match.mealPeriod = "DI"
-        match.day = "15th March, 2019"
-        match.time = "\(i+4) pm"
-        successfulMatches.append(match)
-    }
+
+    var match = SuccessfulMatch(profilePicture: UIImage(), user: String(), firstName: String(), lastName: String(), diningHall: String(), mealPeriod: String(), chatURL: String(), day: String(), time: String())
+    match.user = "User1"
+    match.firstName = "Joe"
+    match.lastName = "Bruin"
+    match.diningHall = "BPlate"
+    match.mealPeriod = "Lunch"
+    match.day = "15th March, 2019"
+    match.time = "1 pm"
+    successfulMatches.append(match)
+        
+    match = SuccessfulMatch(profilePicture: UIImage(), user: String(), firstName: String(), lastName: String(), diningHall: String(), mealPeriod: String(), chatURL: String(), day: String(), time: String())
+    match.user = "User2"
+    match.firstName = "Hannah"
+    match.lastName = "Chen"
+    match.diningHall = "Covel"
+    match.mealPeriod = "Lunch"
+    match.day = "16th March, 2019"
+    match.time = "1 pm"
+    successfulMatches.append(match)
+    
+    match = SuccessfulMatch(profilePicture: UIImage(), user: String(), firstName: String(), lastName: String(), diningHall: String(), mealPeriod: String(), chatURL: String(), day: String(), time: String())
+    match.user = "User3"
+    match.firstName = "Suzzie"
+    match.lastName = "Davis"
+    match.diningHall = "BPlate"
+    match.mealPeriod = "Breakfast"
+    match.day = "17th March, 2019"
+    match.time = "7 am"
+    successfulMatches.append(match)
+    
     completionHandler(successfulMatches)
 }
 
 func getPendingMatches(completionHandler: ([PendingMatch])->Void){
     var pendingMatches = [PendingMatch]()
-    for i in 1...5 {
-        var match = PendingMatch(diningHall: String(), mealPeriod: String(), day: String(), times: String(), status: String())
-        match.diningHall = "BP"
-        match.mealPeriod = "DI"
-        match.day = "15th March, 2019"
-        match.times = "\(i) pm - \(i+2) pm"
-        match.status = "P"
-        pendingMatches.append(match)
-    }
+    
+    var match = PendingMatch(diningHall: String(), mealPeriod: String(), day: String(), times: String(), status: String())
+    match.diningHall = "BPlate"
+    match.mealPeriod = "Dinner"
+    match.day = "15th March, 2019"
+    match.times = "5 pm - 6 pm"
+    match.status = "P"
+    pendingMatches.append(match)
+    
+    match = PendingMatch(diningHall: String(), mealPeriod: String(), day: String(), times: String(), status: String())
+    match.diningHall = "BPlate"
+    match.mealPeriod = "Breakfast"
+    match.day = "16th March, 2019"
+    match.times = "7 am - 8:30 am"
+    match.status = "P"
+    pendingMatches.append(match)
+    
+    match = PendingMatch(diningHall: String(), mealPeriod: String(), day: String(), times: String(), status: String())
+    match.diningHall = "Covel"
+    match.mealPeriod = "Dinner"
+    match.day = "16th March, 2019"
+    match.times = "6:30 pm - 8 pm"
+    match.status = "P"
+    pendingMatches.append(match)
+    
+    match = PendingMatch(diningHall: String(), mealPeriod: String(), day: String(), times: String(), status: String())
+    match.diningHall = "BPlate"
+    match.mealPeriod = "Lunch"
+    match.day = "17th March, 2019"
+    match.times = "12:30 pm - 1:30 pm"
+    match.status = "P"
+    pendingMatches.append(match)
+    
+    match = PendingMatch(diningHall: String(), mealPeriod: String(), day: String(), times: String(), status: String())
+    match.diningHall = "FEAST"
+    match.mealPeriod = "Dinner"
+    match.day = "17th March, 2019"
+    match.times = "6 pm - 6:30 pm"
+    match.status = "P"
+    pendingMatches.append(match)
+    
     completionHandler(pendingMatches)
 }
